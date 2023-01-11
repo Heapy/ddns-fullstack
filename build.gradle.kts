481526b35427aae385dc6d31f9640d384e06d7a7
Ruslan Ibragimov <ruslan@heapy.io>
2023-01-12 02:02:21 +0300

    Kotlin 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
-    kotlin("jvm").version("1.7.22")
-    kotlin("plugin.serialization").version("1.7.22")
+    kotlin("jvm").version("1.8.0")
+    kotlin("plugin.serialization").version("1.8.0")
 }
 
 application {
@@ -14,18 +14,18 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.1")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.2")
 
-    implementation("io.ktor:ktor-client-cio:2.2.1")
-    implementation("io.ktor:ktor-client-content-negotiation:2.2.1")
+    implementation("io.ktor:ktor-client-cio:2.2.2")
+    implementation("io.ktor:ktor-client-content-negotiation:2.2.2")
 
-    implementation("io.ktor:ktor-server-cio:2.2.1")
-    implementation("io.ktor:ktor-server-content-negotiation:2.2.1")
-    implementation("io.ktor:ktor-server-call-logging:2.2.1")
+    implementation("io.ktor:ktor-server-cio:2.2.2")
+    implementation("io.ktor:ktor-server-content-negotiation:2.2.2")
+    implementation("io.ktor:ktor-server-call-logging:2.2.2")
 
     implementation("ch.qos.logback:logback-classic:1.4.5")
 
-    testImplementation("io.ktor:ktor-server-tests:2.2.1")
+    testImplementation("io.ktor:ktor-server-tests:2.2.2")
 
     testImplementation(kotlin("test-junit"))
 }
