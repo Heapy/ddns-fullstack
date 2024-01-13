6b405580eb089f3f155a3ca6e4b6781c81dea409
Ruslan Ibragimov <ruslan@heapy.io>
2024-01-13 11:40:24 +0300

    Add cloudflare worker for fully free DDNS service

---
+++ cloudflare-worker/index.js
@@ -0,0 +1,19 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+    fetch(request, env, ctx) {
+        return new Response(JSON.stringify(
+            {
+                ip: request.headers.get('cf-connecting-ip'),
+            },
+        ))
+    }
+}
